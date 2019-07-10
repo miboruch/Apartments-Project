4d1a6d288e9eefcd4a597522be54283bec89d066
Michał Boruch <miboruch@gmail.com>
2019-07-10 22:10:04 +0200

    Header and hamburger layout

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prettier = require('gulp-prettier');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const merge = require('merge-stream');
+
+gulp.task('update', function(){
+  return gulp.src('src/*')
+  .pipe(gulp.dest('public/'))
+});
+
+gulp.task('sass', function(){
+  return gulp.src('src/Client/scss/*.scss')
+  .pipe(sass().on('error', function(e){
+    console.log(e);
+  }))
+  .pipe(gulp.dest('public/Client/css/'))
+});
+
+gulp.task('prettier', function(){
+  let js = gulp.src('src/Client/js/*.js')
+  .pipe(prettier({singleQuote: true}))
+  .pipe(gulp.dest('public/Client/js/'))
+
+  let server = gulp.src('src/Server/*.js')
+  .pipe(prettier({singleQuote: true}))
+  .pipe(gulp.dest('public/Server/'))
+
+  return merge(js, server);
+});
+
+gulp.task('imagemin', function(){
+  return gulp.src('src/Client/assets/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('public/Client/assets/img/'))
+});
+
+gulp.task('uglify', function(){
+  let js = gulp.src('src/Client/js/*.js')
+  .pipe(uglify().on('error', function(e){
+    console.log(e);
+  }))
+  .pipe(gulp.dest('public/Client/js/'))
+
+  let server = gulp.src('src/Server/*.js')
+  .pipe(uglify().on('error', function(e){
+    console.log(e);
+  }))
+  .pipe(gulp.dest('public/Server/'))
+
+  return merge(js, server);
+});
+
+gulp.task('watch', function(){
+  return gulp.watch('src/Client/scss/*.scss', gulp.series('sass')),
+  gulp.watch('src/Client/js/*.js', gulp.series('prettier')),
+  gulp.watch('public/Server/', gulp.series('prettier')),
+  gulp.watch('src/*', gulp.series('update'));
+})
---
+++ public/Client/js/index.js
@@ -0,0 +1,11 @@
+console.log('hello');
+let button = document.querySelector('.hamburger');
+let slider = document.querySelector('.slider');
+let nav = document.querySelector('.main-nav');
+let inner = document.querySelector('.inner-hamburger');
+
+button.addEventListener('click', function() {
+  slider.classList.toggle('slider--active');
+  nav.classList.toggle('main-nav--open');
+  inner.classList.toggle('inner-hamburger--open');
+});
